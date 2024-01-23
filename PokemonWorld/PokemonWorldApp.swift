25753dd0e17bf852033d83ba619b43fd151b653c
Yaroslav <yaroslav.0029@gmail.com>
2024-01-23 23:25:32 +0100

    create project
    - add gitignore
    - create project

---
+++ PokemonWorld/PokemonWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWorldApp.swift
+//  PokemonWorld
+//
+//  Created by Yaroslav Chlek on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
